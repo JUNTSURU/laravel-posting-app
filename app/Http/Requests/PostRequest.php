ec26dc628608e6bb7956000520378385ac17ad33
JUNTSURU <ad.free.ac.mail@gmail.com>
2024-05-07 11:28:27 +0900

    課題提出

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // コントローラー実行前に、ユーザー権限を確認するもの。デフォルトはfalse
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required',
+            'content' => 'required'
+        ];
+    }
+}
